4440680c6756deaddb15acd831f227f0b89e0081
Dario Caruso <darioyzf@gmail.com>
2015-11-29 15:51:28 +0100

    start

--- index.js
+++ index.js
@@ -3,29 +3,29 @@ Promise=require('promise'),
 verb=require('verbo');
 
 
-function WlSwConf(){
-
-var options={};
-    return new Promise(function(resolve,reject){
-
-    exec('echo -n $(cat /etc/dnsmasq.conf|grep interface=|grep -v "#"|sed \'s/interface=//g\')').then(function(i){
-    options.interface=i;
-
-    }).then(function(){
-      exec('echo -n $(cat /etc/dnsmasq.conf|grep dhcp-range|grep -v "#"|sed "s/dhcp-range=//g"|sed -s "s/,/ /g"|awk \'{print$(1)}\')').then(function(hostIp){
-        options.hostIp=hostIp.split('.')[0]+'.'+hostIp.split('.')[1]+'.'+hostIp.split('.')[2]+'.1'
-    resolve(options)
-      }).catch(function(err){
-        reject(err)
-      })
-    }).catch(function(err){
-      reject(err)
-    })
-
-    })
-
-
-}
+// function WlSwConf(){
+//
+// var options={};
+//     return new Promise(function(resolve,reject){
+//
+//     exec('echo -n $(cat /etc/dnsmasq.conf|grep interface=|grep -v "#"|sed \'s/interface=//g\')').then(function(i){
+//     options.interface=i;
+//
+//     }).then(function(){
+//       exec('echo -n $(cat /etc/dnsmasq.conf|grep dhcp-range|grep -v "#"|sed "s/dhcp-range=//g"|sed -s "s/,/ /g"|awk \'{print$(1)}\')').then(function(hostIp){
+//         options.hostIp=hostIp.split('.')[0]+'.'+hostIp.split('.')[1]+'.'+hostIp.split('.')[2]+'.1'
+//     resolve(options)
+//       }).catch(function(err){
+//         reject(err)
+//       })
+//     }).catch(function(err){
+//       reject(err)
+//     })
+//
+//     })
+//
+//
+// }
 
 
 

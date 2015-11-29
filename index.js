2f3159c4ceb421cb563e060c2d05d7e59e07e1dc
Dario Caruso <darioyzf@gmail.com>
2015-11-29 15:10:00 +0100

    start

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function WlSwConf(){
 
     }).then(function(){
       exec('echo $(cat /etc/dnsmasq.conf|grep dhcp-range|grep -v "#"|sed "s/dhcp-range=//g"|sed -s "s/,/ /g"|awk \'{print$(1)}\'| sed "s/\./ /g"|awk \'{print$(1)}\').$(cat /etc/dnsmasq.conf|grep dhcp-range|grep -v "#"|sed "s/dhcp-range=//g"|sed -s "s/,/ /g"|awk \'{print$(1)}\'| sed "s/\./ /g"|awk \'{print$(2)}\').$(cat /etc/dnsmasq.conf|grep dhcp-range|grep -v "#"|sed "s/dhcp-range=//g"|sed -s "s/,/ /g"|awk \'{print$(1)}\'| sed "s/\./ /g"|awk \'{print$(3)}\').1').then(function(hostIp){
-        WlanSwitch.hostIp=hostIp
+        options.hostIp=hostIp
     resolve(options)
       }).catch(function(err){
         reject(err)

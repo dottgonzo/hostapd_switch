489044e86c28a7f1a5c8b202b0e418ee51501c2a
Dario Caruso <darioyzf@gmail.com>
2015-11-29 16:00:21 +0100

    start

--- tests/configure.js
+++ tests/configure.js
@@ -1,10 +1,11 @@
 var wswitch=require('../index'),
 verb=require('verbo');
-
-wswitch.configure().then(function(options){
-  verb(options,'hostapd_switch')
-
-}).catch(function(err){
-
-  verb(err,'error','hostapd_switch')
-})
+wswitch.configure()
+//
+// wswitch.configure().then(function(options){
+//   verb(options,'hostapd_switch')
+//
+// }).catch(function(err){
+//
+//   verb(err,'error','hostapd_switch')
+// })

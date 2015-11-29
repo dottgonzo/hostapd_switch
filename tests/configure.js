6975d43caf737407ad609c2fac93a20dfa7b89b2
Dario Caruso <darioyzf@gmail.com>
2015-11-29 15:12:27 +0100

    start

--- tests/configure.js
+++ tests/configure.js
@@ -2,7 +2,11 @@ var WlanSwitch=require('../index'),
 verb=require('verbo');
 
 WlanSwitch().then(function(options){
+  console.log('options')
+
   console.log(options)
 }).catch(function(err){
+  console.log('err')
+
   verb(err,'error','hostapd_switch')
 })

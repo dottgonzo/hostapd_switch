afb8b9367b98ea342c2fb1067000db46d6372aac
Dario Caruso <darioyzf@gmail.com>
2015-12-01 14:52:01 +0100

    oki

--- run/ap.js
+++ run/ap.js
@@ -2,7 +2,7 @@ var WlanSwitch=require('../index'),
 verb=require('verbo');
 
 
-WlanSwitch.ap().then(function(options){
+WlanSwitch.ap({wpasupplicant_path:'/etc/wpa_supplicant/wpa_supplicant.conf'}).then(function(options){
   verb(options,'info','hostapd_switch ap')
 }).catch(function(err){
   verb(err,'error','hostapd_switch ap')
--- run/settings.js
+++ run/settings.js
@@ -1,7 +1,7 @@
 var WlanSwitch=require('../index'),
 verb=require('verbo');
 
-WlanSwitch.settings('/etc/wpa_supplicant/wpa_supplicant.conf').then(function(options){
+WlanSwitch.settings({wpasupplicant_path:'/etc/wpa_supplicant/wpa_supplicant.conf'}).then(function(options){
   verb(options,'info','hostapd_switch settings')
 }).catch(function(err){
   verb(err,'error','hostapd_switch settings')

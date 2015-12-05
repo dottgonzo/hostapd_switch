3f1ca101256b32a4c96487d0b350c61dc32c299f
Dario Caruso <darioyzf@gmail.com>
2015-12-05 06:34:32 +0100

    oki

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ HAPDSW.prototype.host=function(){
   var dnsmasq=this.dnsmasq;
   var redirect_port=this.config.redirect;
   var hostIp=dnsmasq.host;
-  var cmd='pkill wpa_supplicant ; ifconfig '+this.config.interface+' up && systemctl start hostapd ; systemctl restart dnsmasq && ifconfig '+this.config.interface+' '+hostIp+' netmask 255.255.255.0 up && sleep 5'
+  var cmd='pkill wpa_supplicant ; ifconfig '+this.config.interface+' up && systemctl restart hostapd ; systemctl restart dnsmasq && ifconfig '+this.config.interface+' '+hostIp+' netmask 255.255.255.0 up && sleep 5'
   return new Promise(function(resolve,reject){
     dnsmasq.setmode('host').then(function(){
 
@@ -146,7 +146,7 @@ HAPDSW.prototype.host=function(){
 HAPDSW.prototype.ap=function(){
   var dnsmasq=this.dnsmasq;
   var hostIp=dnsmasq.host;
-  var cmd='pkill wpa_supplicant ; ifconfig '+this.config.interface+' up  ; systemctl restart dnsmasq && ifconfig '+this.config.interface+' '+hostIp+' netmask 255.255.255.0 up && for i in $( iptables -t nat --line-numbers -L | grep ^[0-9] | awk \'{ print $1 }\' | tac ); do iptables -t nat -D PREROUTING $i; done'
+  var cmd='pkill wpa_supplicant ; ifconfig '+this.config.interface+' up  && systemctl restart hostapd ; systemctl restart dnsmasq && ifconfig '+this.config.interface+' '+hostIp+' netmask 255.255.255.0 up && for i in $( iptables -t nat --line-numbers -L | grep ^[0-9] | awk \'{ print $1 }\' | tac ); do iptables -t nat -D PREROUTING $i; done'
   return new Promise(function(resolve,reject){
     dnsmasq.ap().then(function(){
       exec(cmd).then(function(){

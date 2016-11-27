e7dfe4d2c12a7a3808922799966175f068eb67cc
Dario Caruso <darioyzf@gmail.com>
2016-11-27 22:14:26 +0100

    fix host (only tests was wrong?)

--- run/host.ts
+++ run/host.ts
@@ -11,7 +11,7 @@ conf.redirect = 4000
 const WS = new WlanSwitch(conf, true)
 console.log(WS)
 
-WS.client(true, true).then(function (options) {
+WS.host().then(function (options) {
     verb(options, 'info', 'hostapd_switch host')
 }).catch(function (err) {
     verb(err, 'error', 'hostapd_switch host')

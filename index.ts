187c5c473706c30d867ce5cdd049efe292680532
Dario Caruso <darioyzf@gmail.com>
2016-11-27 21:07:14 +0100

    back

--- index.ts
+++ index.ts
@@ -210,7 +210,7 @@ export default class HostapdSwitch extends wpamanager {
         const that = this
         let dnsmasq = this.dnsmasq;
         let hostIp = dnsmasq.hostIp;
-        let cmd = 'pkill wpa_supplicant ; ifconfig ' + this.config.interface + ' up && systemctl restart hostapd ; systemctl restart dnsmasq && ifconfig ' + this.config.interface + ' ' + hostIp + ' netmask 255.255.255.0 up && sleep 5 && for i in $( iptables -t nat --line-numbers -L | grep ^[0-9] | awk \'{ print $1 }\' | tac ); do iptables -t nat -D PREROUTING $i; done';
+        let cmd = 'pkill wpa_supplicant ; ifconfig ' + this.config.interface + ' up && systemctl restart hostapd ; systemctl restart dnsmasq && ifconfig ' + this.config.interface + ' ' + hostIp + ' netmask 255.255.255.0 up && sleep 5';
         return new Promise<boolean>(function (resolve, reject) {
             dnsmasq.host().then(function () {
 
